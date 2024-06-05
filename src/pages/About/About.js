71f312e492d687c72bf3aa2939c44051de1309a5
Kapil Kandel <kapil@portpro.io>
2024-06-05 18:43:10 +0545

    static html added

---
+++ src/pages/About/About.js
@@ -0,0 +1,3 @@
+export const ABOUT_ME = `
+<p class="description-aboutme pt-2 lh-4">Write your description and life experiences here.</p><p class="description-aboutme-2 pt-2 lh-4">This is for paragraph change and making the para fit with your vertical image beside this.</p><p class="description-aboutme-3 pt-2 lh-4">If you have any questions, please don't hesitate to contact me at<span>manip.poudel@gmail.com</span></p>
+`;
